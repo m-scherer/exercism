49b2582e2435963462b7029aceb5ccedbd7865cc
Michael Scherer <michael.c.scherer@gmail.com>
2017-03-24 15:00:30 -0600

    Refactor rna exercism

--- java/rna-transcription/src/main/java/RnaTranscription.java
+++ java/rna-transcription/src/main/java/RnaTranscription.java
@@ -1,35 +1,21 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-
 public class RnaTranscription {
 
     public String ofDna(String dnaString) {
-        HashMap<String, String> rna = new HashMap<>();
-        rna.put("G", "C");
-        rna.put("C", "G");
-        rna.put("T", "A");
-        rna.put("A", "U");
-        rna.put("", "");
+        if(dnaString.length()==0) return "";
+        StringBuilder DNA = new StringBuilder(dnaString);
 
-        String[] letters = dnaString.split("");
-        List<String> splitString = Arrays.asList(letters);
-
-        System.out.println(rna);
-        System.out.println(splitString);
-
-        ArrayList<String> convertedStrands = new ArrayList<>();
-        for (String letter : splitString ) {
-            convertedStrands.add( rna.get(letter) );
-        }
-
-        StringBuilder newString = new StringBuilder();
-        for (String convertedLetter : convertedStrands) {
-            newString.append(convertedLetter);
-        }
-        String output = newString.toString();
-
-        return output;
+        for (int i = 0; i < DNA.length(); i++)
+            switch( DNA.charAt(i) ) {
+                case 'C':   DNA.setCharAt(i, 'G');
+                            break;
+                case 'G':   DNA.setCharAt(i, 'C');
+                            break;
+                case 'T':   DNA.setCharAt(i, 'A');
+                            break;
+                case 'A':   DNA.setCharAt(i, 'U');
+                            break;
+                default:    break;
+            }
+        return DNA.toString();
     }
 }

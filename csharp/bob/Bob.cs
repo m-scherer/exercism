708d0751182b81466d8b6721d9c8bf6773c0da8c
Michael Scherer <michael.c.scherer@gmail.com>
2017-04-17 11:32:19 -0600

    Complete bob exercism in c#

---
+++ csharp/bob/Bob.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+public static class Bob
+{
+    public static string Hey(string statement)
+    {
+		string trimStatement = statement.TrimEnd();
+
+		if (trimStatement == "") {
+			return "Fine. Be that way!";
+		} else if (trimStatement.ToUpper() == trimStatement && Regex.Match(trimStatement,@"[A-Z]").Success) {
+			return "Whoa, chill out!";
+		} else if (trimStatement.Substring(trimStatement.Length - 1) == "?") {
+			return "Sure.";
+		} else if (trimStatement.ToUpper() == trimStatement && trimStatement.ToLower() == trimStatement){
+			return "Whatever.";	
+		} else {
+			return "Whatever.";
+		}
+
+    }
+}

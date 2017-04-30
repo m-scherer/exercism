5ab8e1861e7569c39f73c8b619e2ceec1dce1831
Michael Scherer <michael.c.scherer@gmail.com>
2017-04-29 20:12:58 -0600

    Minor refactor on sieve

--- javascript/sieve/sieve.js
+++ javascript/sieve/sieve.js
@@ -11,13 +11,13 @@ class Sieve {
     if (position >= range.length) {
       return range;
     }
-      for (let i = 0; i < range.length; i++) {
-        if (range[i] % range[position] == 0 && range[i] != range[position]) {
-          range.splice(i, 1);
-        }
+    for (let i = 0; i < range.length; i++) {
+      if (range[i] % range[position] == 0 && range[i] != range[position]) {
+        range.splice(i, 1);
       }
-      position += 1;
-      return this.getPrimes(range, position)
+    }
+    position += 1;
+    return this.getPrimes(range, position)
   }
 
   createRange() {

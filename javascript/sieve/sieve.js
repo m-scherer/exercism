51701ab5106f0a0b92d2e76a39fd94ad4b74d213
Michael Scherer <michael.c.scherer@gmail.com>
2017-04-29 20:10:43 -0600

    Complete Sieve in Javascript

---
+++ javascript/sieve/sieve.js
@@ -0,0 +1,36 @@
+class Sieve {
+  constructor(number){
+    this.number = number;
+  }
+
+  get primes() {
+    return this.getPrimes(this.createRange(), 0);
+  }
+
+  getPrimes(range, position) {
+    if (position >= range.length) {
+      return range;
+    }
+      for (let i = 0; i < range.length; i++) {
+        if (range[i] % range[position] == 0 && range[i] != range[position]) {
+          range.splice(i, 1);
+        }
+      }
+      position += 1;
+      return this.getPrimes(range, position)
+  }
+
+  createRange() {
+    let range = [];
+    let counter = 2
+
+    while (counter <= this.number) {
+      range.push(counter);
+      counter += 1;
+    }
+    return range;
+  }
+
+}
+
+module.exports = Sieve;
